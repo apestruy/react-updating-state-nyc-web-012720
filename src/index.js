d5fe7b61a37806bb9f28e540a1f19d7ac5178fad
Alexandra Pestruyeva <apestruy@gmail.com>
2020-03-30 17:38:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ClickityClick from "./components/ClickityClick";
 
 ReactDOM.render(
   <div>
-    
+    <ClickityClick />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
